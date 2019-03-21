b1aab7fd526ec3f0d1c94f536e21a8a2b355b492
Erdem Avni Selçuk [MAC] <eravse@gmail.com>
2019-03-21 16:01:50 +0300

    Release Service V1
    Ready to use is the first available version.

---
+++ Setup/uninstall_kapitto_service.bat
@@ -0,0 +1,12 @@
+@e@ECHO OFF
+
+REM The following directory is for .NET 4.0
+set DOTNETFX2=%SystemRoot%\Microsoft.NET\Framework64\v4.0.30319
+set PATH=%PATH%;%DOTNETFX2%
+
+echo Installing IEPPAMS Win Service...
+echo ---------------------------------------------------
+C:\Windows\Microsoft.NET\Framework64\v4.0.30319\installutil.exe -u "C:\Services\Profect.LogPusher.PingerWinService\Profect.LogPusher.PingerWinService.exe"
+echo ---------------------------------------------------
+pause
+echo Done.
